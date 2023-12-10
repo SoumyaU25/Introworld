996fda1b3ae3dd1a1e12ed47e646f338af301baa
Soumya Upadhyay <99285192+SoumyaU25@users.noreply.github.com>
2023-12-10 12:02:25 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,4 @@
-// module.exports={
-//     MONGOURI : "mongodb+srv://soumya:KKaxW6uTGxHgPuHz@cluster0.h1atirr.mongodb.net/?retryWrites=true&w=majority",
-//     JWT_TOK : "abcdefgh"
-// }
+
 
 if(process.env.NODE_ENV=='production'){
     module.exports = require('./prod')
